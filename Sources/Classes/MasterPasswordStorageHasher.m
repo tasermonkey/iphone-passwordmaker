180046dff0f87a732715fb9f735a20a1fea86fdc
James Stapleton <github@tasermonkeys.com>
2010-06-19 09:59:50 -0400

    The headers for the 2 files should have been the license agreement

--- Sources/Classes/MasterPasswordStorageHasher.m
+++ Sources/Classes/MasterPasswordStorageHasher.m
@@ -1,10 +1,21 @@
-//
-//  MasterPasswordStorageHasher.m
-//  PasswordMaker
-//
-//  Created by James Stapleton on 6/17/10.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
-//
+/* 
+ * Copyright (C) 2010  James Stapleton
+ *
+ * This file is part of Iphone PasswordMaker.
+ *
+ * Iphone PasswordMaker is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Iphone PasswordMaker is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with Iphone PasswordMaker.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 #import "MasterPasswordStorageHasher.h"
 
